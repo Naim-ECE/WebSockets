416f72935c3e6974ed05c4e843473ec49132c075
Md. Naimur Rahman Naim <naimur2210026@gmail.com>
2026-08-20 21:54:10 +0600

    implement websocket server

--- Sportz/src/index.js
+++ Sportz/src/index.js
@@ -1,10 +1,19 @@
 import express from "express";
 import { matchesRouter } from "./routes/matches.js";
+import { attachWebSocketServer } from "./ws/server.js";
+import http from "http";
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
-const PORT = 8000;
+const PORT = Number(process.env.PORT) || 8000;
+const HOST = process.env.HOST || "0.0.0.0";
 
 app.use(express.json());
+const server = http.createServer(app);
+
+const { broadcastMatchCreated } = attachWebSocketServer(server);
+app.locals.broadcastMatchCreated = broadcastMatchCreated;
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to the Sportz WebSocket server!" });
@@ -12,6 +21,11 @@ app.get("/", (req, res) => {
 
 app.use("/matches", matchesRouter);
 
-app.listen(PORT, () => {
-  console.log(`Server started at http://localhost:${PORT}`);
+server.listen(PORT, HOST, () => {
+  const baseUrl =
+    HOST === "0.0.0.0" ? `http://localhost:${PORT}` : `http://${HOST}:${PORT}`;
+  console.log(`Server is running on ${baseUrl}`);
+  console.log(
+    `WebSocket server is running on ${baseUrl.replace("http", "ws")}/ws`,
+  ); // this is the WebSocket server URL
 });
